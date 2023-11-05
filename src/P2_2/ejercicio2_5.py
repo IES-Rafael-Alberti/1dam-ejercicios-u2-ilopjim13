28f0047c272a11839de77a30b4c1706bf28e4b16
ivanjim13 <ivangorrion57@gmail.com>
2023-11-05 20:38:16 +0100

    P2_2 terminado

--- src/P2_2/ejercicio2_5.py
+++ src/P2_2/ejercicio2_5.py
@@ -9,15 +9,17 @@
 
 def inversion(amount, interest, tiempo):
     cont = 0
+    num = 0
     while cont < tiempo:
         amount *= 1 + interest / 100
-        print(amount)
+        num += 1
+        print("Año {}: {:.2f}€".format(num,amount))
         cont += 1
 
 def main():
     amount = int(input("Introduce la cantidad a invertir: "))
     interest = int(input("Introduce el interes anual: "))
-    tiempo = int(input("Introduce el interes anual: "))
+    tiempo = int(input("Introduce los años: "))
     inversion(amount, interest, tiempo)
 
 if __name__ == "__main__":
